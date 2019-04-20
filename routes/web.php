320822262835b35bd8006cae1a22f2254d8682ac
wangleidexiangmu <1109073586@qq.com>
2019-04-20 02:20:55 +0000

    bug

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,6 @@ Route::get('/order/create', 'Order\IndexController@create');      //生成订单
 Route::get('/order/paystatus', 'Order\IndexController@payStatus');      //查询订单支付状态
 //微信支付
 Route::get('/pay/weixin', 'Weixin\WxPayController@pay');      //微信支付
-Route::post('/weixin/pay/notify', 'Weixin\PayController@notify');      //微信支付通知回调
+Route::post('/weixin/pay/notify', 'Weixin\WxPayController@notify');      //微信支付通知回调
 //支付
 Route::get('/pay/success', 'Weixin\PayController@paySuccess');      //微信支付成功
