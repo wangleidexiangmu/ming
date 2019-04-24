275a15653791dbba2785ec3ca37d044c3856ff99
wangleidexiangmu <1109073586@qq.com>
2019-04-24 11:30:16 +0000

    bug

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -145,15 +145,15 @@ class WeixinController extends Controller
     }
     public function geturl(){
         echo '<pre>';print_r($_GET);echo '</pre>';
-        $code = $_GET['code'];
-        $token=' https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APP_ID').'&secret='.env('WX_APP_SEC').'&code='.$code.'&grant_type=authorization_code';
-        $response = json_decode(file_get_contents($token),true);
-        echo '<pre>';print_r($response);echo '</pre>';
-        $access_token = $response['access_token'];
-        $openid = $response['openid'];
-        //获取用户信息
-        $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
-        $user_info = json_decode(file_get_contents($url),true);
-        echo '<pre>';print_r($user_info);echo '</pre>';
+//        $code = $_GET['code'];
+//        $token=' https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APP_ID').'&secret='.env('WX_APP_SEC').'&code='.$code.'&grant_type=authorization_code';
+//        $response = json_decode(file_get_contents($token),true);
+//        echo '<pre>';print_r($response);echo '</pre>';
+//        $access_token = $response['access_token'];
+//        $openid = $response['openid'];
+//        //获取用户信息
+//        $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+//        $user_info = json_decode(file_get_contents($url),true);
+//        echo '<pre>';print_r($user_info);echo '</pre>';
 }
 }
