f98e70cfc523ecd5dc06c4ae793ed75848d1de63
wanglei <1109073586@qq.com>
2019-04-25 08:27:30 +0800

    存库

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -8,6 +8,7 @@ use App\Model\weixin\txt;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
 use App\Model\GoodsModel;
+use App\Model\info;
 use Illuminate\Support\Str;
 class WeixinController extends Controller
 {
@@ -141,19 +142,59 @@ class WeixinController extends Controller
 
     public function prower(){
        $url=urlEncode('http://1809wanglei.comcto.com/geturl');
+      // return $url;
         $code='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('WX_APP_ID').'&redirect_uri='.$url.'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect ';
+       /// return $code;
     }
     public function geturl(){
         echo '<pre>';print_r($_GET);echo '</pre>';
-//        $code = $_GET['code'];
-//        $token=' https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APP_ID').'&secret='.env('WX_APP_SEC').'&code='.$code.'&grant_type=authorization_code';
-//        $response = json_decode(file_get_contents($token),true);
-//        echo '<pre>';print_r($response);echo '</pre>';
-//        $access_token = $response['access_token'];
-//        $openid = $response['openid'];
-//        //获取用户信息
-//        $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
-//        $user_info = json_decode(file_get_contents($url),true);
-//        echo '<pre>';print_r($user_info);echo '</pre>';
+        $code = $_GET['code'];
+        //获取 access_token
+        $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APP_ID').'&secret='.env('WX_APP_SEC').'&code='.$code.'&grant_type=authorization_code';
+        $response = json_decode(file_get_contents($url),true);
+        echo '<pre>';print_r($response);echo '</pre>';
+        $access_token = $response['access_token'];
+        $openid = $response['openid'];
+        //获取用户信息
+        $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $user_info = json_decode(file_get_contents($url),true);
+        echo '<pre>';print_r($user_info);echo '</pre>';
+        $openid=$user_info['openid'];
+        $wx_id='oYL3b5krtrmqxlwXs0A_7cv4vaJg';
+       $res= info::where(['openid'=>$user_info['openid']])->first();
+     // echo $res;exit;
+        if($res){
+            echo '
+<xml>
+<ToUserName><![CDATA[' . $openid . ']]></ToUserName>
+<FromUserName><![CDATA['.$wx_id.']]></FromUserName>
+<CreateTime>' . time() . '</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[' . '欢迎回来 ' . $user_info['nickname'] . ']]></Content>
+</xml>';
+            echo '欢迎回来';
+        }else{
+            $info=[
+                'openid'=>$user_info['openid'],
+                'nickname'=>$user_info['nickname'],
+                'sex'=>$user_info['sex'],
+                'language'=>$user_info['language'],
+                'city'=>$user_info['city'],
+                'province'=>$user_info['province'],
+                'country'=>$user_info['country'],
+                'head'=>$user_info['headimgurl']
+            ];
+            info::insert($info);
+            echo '
+<xml>
+<ToUserName><![CDATA[' . $openid . ']]></ToUserName>
+<FromUserName><![CDATA['.$wx_id.']]></FromUserName>
+<CreateTime>' . time() . '</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[' . '欢迎关注 ' . $user_info['nickname'] . ']]></Content>
+</xml>';
+            echo '欢迎关注';
+        }
+
 }
 }
