e9a873814940491455bfde1c7981388a889cacda
wangleidexiangmu <1109073586@qq.com>
2019-04-28 13:15:54 +0000

    bug

--- app/Http/Controllers/Weixin/SOController.php
+++ app/Http/Controllers/Weixin/SOController.php
@@ -29,4 +29,23 @@ class SOController extends Controller
     public function puball(){
         return view('weixin.js');
     }
+    public function jstest(){
+        //计算签名
+        $nonceStr = Str::random(10);
+        $ticket = getJsapiTicket();
+        $timestamp = time();
+        $current_url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] .$_SERVER['REQUEST_URI'];
+        $string1 = "jsapi_ticket=$ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$current_url";
+        $sign = sha1($string1);
+        $js_config = [
+            'appId' => env('WX_APP_ID'),        //公众号APPID
+            'timestamp' => $timestamp,
+            'nonceStr' => $nonceStr,   //随机字符串
+            'signature' => $sign,                      //签名
+        ];
+        $data = [
+            'jsconfig'  => $js_config
+        ];
+        return view('weixin.js',$data);
+    }
 }
