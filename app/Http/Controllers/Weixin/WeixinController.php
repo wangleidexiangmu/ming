3b87490b1f1ee7fd72ee573fc94ade618b709fe0
wangleidexiangmu <1109073586@qq.com>
2019-04-29 07:36:41 +0000

    bug

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -335,7 +335,7 @@ echo $res;
                 [
                     "type"=>"view",
                     "name"=>"最新福利",
-                    "url"=>"http://1809wanglei.comcto.com/url",
+                    "url"=>"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe750a38a8fe84b93&redirect_uri=http%3A%2F%2F1809wanglei.comcto.com%2Fgeturl&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect",
                 ],
 
             ] ,
@@ -357,7 +357,7 @@ echo $res;
 
 
     }
-    public function url(){
-        $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe750a38a8fe84b93&redirect_uri=http%3A%2F%2F1809wanglei.comcto.com%2Fgeturl&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-    }
+//    public function url(){
+//        $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe750a38a8fe84b93&redirect_uri=http%3A%2F%2F1809wanglei.comcto.com%2Fgeturl&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+//    }
 }
